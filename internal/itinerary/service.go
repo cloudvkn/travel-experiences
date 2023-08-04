ce6c49e3b98b2895afda047d20330b7a3774853a
cloudvkn <141435651+cloudvkn@users.noreply.github.com>
2023-08-04 21:13:00 +0530

    Create service.go

---
+++ internal/itinerary/service.go
@@ -0,0 +1,9 @@
+package itinerary
+
+type ItineraryServiceImplementation struct {
+    // Implement itinerary service methods here
+}
+
+func NewItineraryService() ItineraryService {
+    return &ItineraryServiceImplementation{}
+}
