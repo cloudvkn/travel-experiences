888cf4cd79cf7db7af857ccfcaf448355a3ea75a
cloudvkn <141435651+cloudvkn@users.noreply.github.com>
2023-08-04 21:13:22 +0530

    Create recommendations.go

---
+++ internal/recommendations/recommendations.go
@@ -0,0 +1,10 @@
+package recommendations
+
+type Recommendations struct {
+    // Define recommendations related data models here
+}
+
+type RecommendationsService interface {
+    // Define recommendations service methods here
+}
+
