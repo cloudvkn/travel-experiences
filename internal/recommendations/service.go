d2179b828270e912c09b6edfbe59e03ba55a2647
cloudvkn <141435651+cloudvkn@users.noreply.github.com>
2023-08-04 21:14:23 +0530

    Create service.go

---
+++ internal/recommendations/service.go
@@ -0,0 +1,9 @@
+package recommendations
+
+type RecommendationsServiceImplementation struct {
+    // Implement recommendations service methods here
+}
+
+func NewRecommendationsService() RecommendationsService {
+    return &RecommendationsServiceImplementation{}
+}
