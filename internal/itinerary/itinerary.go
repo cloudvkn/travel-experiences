35b294d3eb624884f23425623405e96f4745c09a
cloudvkn <141435651+cloudvkn@users.noreply.github.com>
2023-08-04 21:12:09 +0530

    Create itinerary.go

---
+++ internal/itinerary/itinerary.go
@@ -0,0 +1,10 @@
+package itinerary
+
+type Itinerary struct {
+    // Define itinerary related data models here
+}
+
+type ItineraryService interface {
+    // Define itinerary service methods here
+}
+
